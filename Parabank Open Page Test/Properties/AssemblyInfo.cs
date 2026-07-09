b1ccf3a5e96ee8e03e62a7298dd5cdd30b5c2b31
Stefan Piperkov <stefanpiperkov96@gmail.com>
2026-07-09 15:45:13 +0300

    Initial commit

---
+++ Parabank Open Page Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Parabank Open Page Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Parabank Open Page Test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("433f901f-4120-4677-acba-77a70c0c885a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Parabank Open Page Test/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParabankAutomation.Tests
+{
+    public class Tests
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+        }
+
+        [Test]
+        public void Test1()
+        {
+           
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver?.Dispose();
+        }
+    }
+}
